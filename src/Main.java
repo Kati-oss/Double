bcf5027e98af3e60cc8646c960cb918fe0559906
Kati <mayssketii@gmail.com>
2025-03-30 06:07:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int eaters = 5; // сколько людей будут есть
+
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((3000 / 5) + " миллилитров(а) воды");
+        System.out.println((5 / 5) + " картофелин(а/ы)");
+        System.out.println((6 / 5) + " куриных(ое) бёдер(ро)");
+        System.out.println((10 / 5) + " ложек(ки/ка) специй");
+
+    }
+}
